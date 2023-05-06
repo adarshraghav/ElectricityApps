2bb89accadf5d09d3e21f169fd9a344f2f5b8b28
adarsh-raghav <aadarshraghav@gmail.com>
2023-05-06 10:18:24 +0530

    Initial Commit

---
+++ Realm ToDo-1/Realm_ToDo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_ToDo_1App.swift
+//  Realm ToDo-1
+//
+//  Created by Adarsh Raghav on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Realm_ToDo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
